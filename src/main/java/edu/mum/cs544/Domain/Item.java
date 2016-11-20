0e2e0c1e85b5e4e5d0d0a2e978d7d18c81140493
Laith Mihyar <laith.mihyar93@gmail.com>
2016-11-19 21:05:01 -0600

    Create Item , itemcategory and status -- Laith Mihyar

--- src/main/java/edu/mum/cs544/Domain/Item.java
+++ src/main/java/edu/mum/cs544/Domain/Item.java
@@ -1,5 +1,104 @@
 package edu.mum.cs544.Domain;
 
-public class Item {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	private int itemId;
+	private String itemName;
+	private String itemDescription;
+	private double itemPrice;
+	@Enumerated
+	private ItemCategory itemCategory;
+	@Enumerated
+	private ItemStatus itemStatus;
+	private int itemQuantity;
+	// lend /rent for (x) # of days
+	private int numofDays;
+	private User owner;
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+
+	public double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public ItemCategory getItemCategory() {
+		return itemCategory;
+	}
+
+	public void setItemCategory(ItemCategory itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+
+	public ItemStatus getItemStatus() {
+		return itemStatus;
+	}
+
+	public void setItemStatus(ItemStatus itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	public int getNumofDays() {
+		return numofDays;
+	}
+
+	public void setNumofDays(int numofDays) {
+		this.numofDays = numofDays;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
 
 }
